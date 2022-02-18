a8f2d5a97cf04dd04afcaa0f0f68600e9bbfb59a
MutsuoYoshiichi <411.mutsuo@gmail.com>
2022-02-18 14:06:17 +0900

    first commit

---
+++ next.config.js
@@ -0,0 +1,25 @@
+const { PHASE_DEVELOPMENT_SERVER, } = require('next/constants')
+
+module.exports = (phase) => {
+
+	if (phase === PHASE_DEVELOPMENT_SERVER) {
+		return {
+			env: {
+				mongodb_username: 'mutsuo',
+				mongodb_password: 'timespacer817',
+				mongodb_clustername: 'cluster0',
+				mongodb_database: 'myFirstDatabase-dev',
+			},
+		}
+	}
+
+	return {
+		env: {
+			mongodb_username: 'mutsuo',
+			mongodb_password: 'timespacer817',
+			mongodb_clustername: 'cluster0',
+			mongodb_database: 'myFirstDatabase',
+		},
+	}
+
+};
